b5b04392215b1b3758fdddf761726b296f73d6d8
tky0084 <106537623+tky0084@users.noreply.github.com>
2022-12-19 01:23:53 +0900

    ProductStore 商品名非表示

--- resources/views/owner/Products/create.blade.php
+++ resources/views/owner/Products/create.blade.php
@@ -63,7 +63,7 @@
 
                           <div class="p-2 w-1/2 mx-auto">
                             <div class="relative">
-                            <label for="shop_id" id="shop_id" class="leading-7 text-sm text-gray-600">カテゴリー ※必須</label>
+                            <label for="category" id="category" class="leading-7 text-sm text-gray-600">カテゴリー ※必須</label>
                             <select  name="category" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"> 
                                 @foreach($categories as $category) 
                               <optgroup label="{{ $category->name }}"> 
