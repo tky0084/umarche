dfee731abbfa9e10c76d0ce68718455fa171aa9b
tky0084 <106537623+tky0084@users.noreply.github.com>
2023-01-07 13:35:00 +0900

    非同期送信

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\Testmail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -32,8 +33,13 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         //dd($request);
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+
+        //同期的に送信
+        //Mail::to('test@example.com')
+        //->send(new TestMail());
+
+        //非同期に送信
+        SendThanksMail::dispatch(); 
 
         $categories = PrimaryCategory::with('secondary')->get();
         
